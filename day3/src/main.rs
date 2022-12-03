7545ac9352888cfb9e6a5d7029bde248e1aa2e6d
Stephane Albanese <albanese.stephane@gmail.com>
2022-12-03 12:25:26 +0100

    Day 3 part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,5 +1,19 @@
 use std::{fs::File, io::BufRead};
 
+fn count(priorities: &Vec<char>) -> usize {
+    priorities.iter().map(|item| {
+        match item {
+            'a'..='z' => {
+                *item as usize - 96
+            },
+            'A'..='Z' => {
+                *item as usize - 64 + 26
+            },
+            _ => 0
+        }
+    }).sum()
+}
+
 fn main() {
     // Part 1
     let mut priorities_pt1: Vec<char> = Vec::new();
@@ -17,17 +31,26 @@ fn main() {
             }
         }
     }
-
-    let total_pt1: usize = priorities_pt1.iter().map(|item| {
-        match item {
-            'a'..='z' => {
-                *item as usize - 96
-            },
-            'A'..='Z' => {
-                *item as usize - 64 + 26
-            },
-            _ => 0
+    
+    let mut priorities_pt2: Vec<char> = Vec::new();
+    let mut group: Vec<Vec<char>> = Vec::new();
+    for line in std::io::BufReader::new(File::open("input.txt").unwrap())
+        .lines()
+        .flatten()
+    {
+        if group.len() == 2 {
+            let mut current_line: Vec<char> = line.chars().collect();
+            current_line.retain(|&item| {
+                group.get(0).unwrap().contains(&item) && group.get(1).unwrap().contains(&item)
+            });
+            current_line.dedup();
+            priorities_pt2.append(&mut current_line);
+            group.clear();
+        } else {
+            group.push(line.chars().collect());
         }
-    }).sum();
-    println!("Total of duplicates = {}", total_pt1);
+    }
+
+    println!("Total of priorities for part 1 = {}", count(&priorities_pt1));
+    println!("Total of grouped priorities for part 2 = {}", count(&priorities_pt2));
 }
