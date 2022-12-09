2a107704284bb3938c7fd8f027e8137d37669ff8
Stephane Albanese <stephane.albanese@arturia.com>
2022-12-09 14:52:46 +0100

    Day 9 - Part 2 + refacto part 1

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,6 +1,6 @@
-use std::collections::HashSet;
+use std::{collections::HashSet};
 
-#[derive(Hash,PartialEq, Eq, Clone,Debug)]
+#[derive(Hash, PartialEq, Eq, Clone, Debug)]
 struct Point {
     x: i32,
     y: i32,
@@ -17,9 +17,9 @@ impl Point {
             'D' => self.translate_y(1),
             'L' => self.translate_x(-1),
             'R' => self.translate_x(1),
-            _ => ()
+            _ => (),
         }
-    } 
+    }
 
     fn translate_x(&mut self, delta: i32) {
         self.x += delta;
@@ -32,38 +32,53 @@ impl Point {
     fn get_closer(&mut self, other: &Point) {
         if (other.x - self.x).abs() >= 1 {
             self.translate_x(if other.x > self.x { 1 } else { -1 });
-        } 
+        }
         if (other.y - self.y).abs() >= 1 {
             self.translate_y(if other.y > self.y { 1 } else { -1 });
         }
-    } 
+    }
 
     fn is_too_far(&self, other: &Point) -> bool {
         (other.x - self.x).abs() > 1 || (other.y - self.y).abs() > 1
     }
 }
 
-fn main() {
-    let mut t = Point::new(0, 0);
-    let mut h = Point::new(0, 0);
+fn nb_visited(input: &str, nb_knots: usize) -> usize {
     // Init the visited positions with the initial position.
-    let mut visited: HashSet<Point> = HashSet::from([h.clone()]);
-
-    const INPUT: &str = include_str!("../input.txt");
-
-    // Part 1 : The rope has only one head and one tail.
-    INPUT.lines().for_each(|line| {
-        let instruction = line.chars().nth(0).unwrap();
-        let moves = line.split_whitespace().nth(1).unwrap().parse::<u8>().unwrap();
+    let mut visited: HashSet<Point> = HashSet::from([Point::new(0, 0)]);
+    // The rope consists of n knots all starting at the origin.
+    let mut knots = vec![Point::new(0, 0); nb_knots];
+    input.lines().for_each(|line| {
+        let instruction = line.chars().next().unwrap();
+        let moves = line
+            .split_whitespace()
+            .nth(1)
+            .unwrap()
+            .parse::<u8>()
+            .unwrap();
 
         for _ in 0..moves {
-            h.translate(&instruction);
-            if t.is_too_far(&h) {
-                t.get_closer(&h);
-                visited.insert(t.clone());
+            // Move the head first following the instruction
+            knots.get_mut(0).unwrap().translate(&instruction);
+            // Then iterate all the remaining knots to move them according to the previous one.
+            for k in 1..knots.len() {
+                let prev_knot = &knots[k-1].clone();
+                if knots[k].is_too_far(prev_knot) {
+                    knots[k].get_closer(prev_knot);
+                }
+                // Add the last knot to the set of visited positions.
+                if k == knots.len() - 1 {
+                    visited.insert(knots.last().unwrap().clone());
+                }
             }
         }
     });
+    visited.len()
+}
 
-    println!("Nb visited on part 1 : {}", visited.len());
+fn main() {
+    const INPUT: &str = include_str!("../input.txt");
+
+    println!("Nb visited on part 1 : {}", nb_visited(INPUT, 2));
+    println!("Nb visited on part 2 : {}", nb_visited(INPUT, 10));
 }
